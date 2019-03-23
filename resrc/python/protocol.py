37511d5f0b6388a1498beaaa960fea0b4a667392
ventaquil <konrad.golawski@gmail.com>
2019-03-23 19:04:03 +0100

    Refactor Python code

--- resrc/python/protocol.py
+++ resrc/python/protocol.py
@@ -1,15 +1,18 @@
 from sage.all import *
 
+
 def get_random_prime(b):
     lower_bound = (2 ** (b - 1)) - 1
     higher_bound = (2 ** b) - 1
     prime = random_prime(higher_bound, lbound=lower_bound)
     return prime
 
+
 def get_random_field(b):
     prime = get_random_prime(b)
     field = GF(prime)
     return field
+
 
 def get_random_elliptic_curve(field):
     # TODO what if field is prime
@@ -21,6 +24,7 @@ def get_random_elliptic_curve(field):
             return E
         except ArithmeticError:
             pass
+
 
 def get_algorithm_parameters(elliptic_curve, p):
     while True:
@@ -39,6 +43,7 @@ def get_algorithm_parameters(elliptic_curve, p):
         if k != 1:
             return (n, k)
 
+
 def get_point_of_order(elliptic_curve, p, n, k):
     G = GF(p ** k, "a")
 
@@ -53,8 +58,9 @@ def get_point_of_order(elliptic_curve, p, n, k):
     m = Q.order()
     if m != n:
         Q = Integer(m / n) * Q
-    
+
     return Q
+
 
 def get_asymmetric_key(base_field, point):
     # TODO validate that point is not zero
@@ -63,6 +69,7 @@ def get_asymmetric_key(base_field, point):
         public = secret * point
         if not public.is_zero():
             return (secret, public)
+
 
 def get_3dh_key(secret, public_a, public_b, n, k):
     return public_a.tate_pairing(public_b, n, k) ** secret
